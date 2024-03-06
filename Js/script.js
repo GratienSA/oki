a4c2f79f3bec5a99b32a67160feacd329f59aa74
Daorse <mmorpgplays5@gmail.com>
2024-03-06 20:21:37 +0100

    .

--- Js/script.js
+++ Js/script.js
@@ -3,11 +3,17 @@ let firstLet = document.getElementsByClassName("logo");
 let header = document.querySelector("header");
 let body = document.body;
 let box1 = document.querySelector(".container2");
+let box2 = document.querySelector(".container3");
+let box3 = document.querySelector(".container-user");
+let titrebox = document.querySelectorAll("texte");
 
-console.log(header)
 
 function darkmode() {
     header.classList.toggle("logow");
     body.classList.toggle("bg-black");
     box1.classList.toggle("containerc2");
+    box2.classList.toggle("containerc2");
+    box3.classList.toggle("containerc2");
+    titrebox[i].classList.toggle("Titrew");
 }
+console.log(titrebox)
