9cf382cae49a6f48d731cae968976f2adb3d7d15
Daorse <mmorpgplays5@gmail.com>
2024-03-06 16:59:56 +0100

    .

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,13 @@
 // Passage en mode Nuit
+let firstLet = document.getElementsByClassName("logo");
+let header = document.querySelector("header");
+let body = document.body;
+let box1 = document.querySelector(".container2");
+
+console.log(header)
+
 function darkmode() {
-    let body = document.body;
+    header.classList.toggle("logow");
     body.classList.toggle("bg-black");
+    box1.classList.toggle("containerc2");
 }
